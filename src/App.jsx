118b495ea54b3c4063399b414d73187316bde7bc
Badriddin <badriddinnabiev777@gmail.com>
2026-09-05 09:24:55 +0500

    fix: match capital C in Components path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import MainPage from "./components/pages/MainPage.jsx";
-import ModulesPage from "./components/pages/ModulesPage.jsx";
+import MainPage from "./Components/pages/MainPage";
+import ModulesPage from "./Components/pages/ModulesPage";
 
 function App() {
   return (
